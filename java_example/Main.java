95c0bb09bfcb6546531f8a15b1ca859edbfc03dc
crgimenes <crg@crg.eti.br>
2022-11-29 16:02:44 -0300

    fix url

--- java_example/Main.java
+++ java_example/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 class Main {
 
 	public static void main(String[] args) throws IOException {
-		URL url = new URL("https://testnetapigateway.herokuapp.com");
+		URL url = new URL("https://testnetapigateway.herokuapp.com/test-auth");
 		HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
 		httpConn.setRequestMethod("GET");
 
